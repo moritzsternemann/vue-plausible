602cdc04aca2cc246ee1d0d7815e6d89a6707d76
Till Sanders <mail@till-sanders.de>
2020-12-15 00:32:52 +0100

    Exempt nuxt plugin from SSR
    This caused problems in my Nuxt installation resulting in the error message `history is not defined` originating in `plausible-tracker/src/lib/tracker.ts:240:31`.

--- src/nuxt-module.ts
+++ src/nuxt-module.ts
@@ -5,7 +5,8 @@ import { PlausibleOptions } from 'plausible-tracker'
 const PlausibleModule: Module<PlausibleOptions> = function (moduleOptions) {
   this.addPlugin({
     src: path.resolve(__dirname, './nuxt-plugin.js'),
-    options: moduleOptions
+    options: moduleOptions,
+    ssr: false
   })
 }
 
